211a773d5907ff8938cf40370dc0e031df1930f8
Vimal-45 <dvimal45@gmail.com>
2023-11-09 18:11:33 +0530

    add all links

--- index.js
+++ index.js
@@ -11,8 +11,19 @@ app.use(express.json());
 
 //api routers
 app.get('/', (req, res) => {
-    res.send('Welcome to the Hall Booking API');
-  });  
+  const welcomeMessage = 'Welcome to the Hall Booking API';
+  const apiLinks = `
+    <ul>
+      <li><a href="/api/hall/rooms">Get Room Details</a></li>
+      <li><a href="/api/hall/create/room">Create Room</a></li>
+      <li><a href="/api/hall/booking">Book a Room</a></li>
+      <li><a href="/api/hall/rooms/alldata">List All Data with Booking Status</a></li>
+      <li><a href="/api/hall/customers/alldata">List Customers with All Data</a></li>
+      <li><a href="/api/hall/count/alldata">Booking Count</a></li>
+    </ul>
+  `;
+  res.send(`${welcomeMessage}<br><br>${apiLinks}`);
+});
 
 app.use('/api/hall',router)
 
