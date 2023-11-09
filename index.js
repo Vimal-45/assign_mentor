90c20377c5c10e5f2fa212656adbe6f1ab0b9f47
Vimal-45 <dvimal45@gmail.com>
2023-11-09 19:16:07 +0530

    Origingal link added

--- index.js
+++ index.js
@@ -21,8 +21,19 @@ app.get('/', (req, res) => {
       <li><a href="/api/hall/customers/alldata">List Customers with All Data</a></li>
       <li><a href="/api/hall/count/alldata">Booking Count</a></li>
     </ul>
+    <div>
+  <h1>Please use blelow API's</h1>
+  <p>Get room details - https://hall-booking-n72k.onrender.com/api/hall/rooms</p>
+  <p>create Room      - https://hall-booking-n72k.onrender.com/api/hall/create/room</p>
+  <p>Booking Room     - https://hall-booking-n72k.onrender.com/api/hall/booking</p>
+  <p>ListAllDataWithbookingStatus- https://hall-booking-n72k.onrender.com/api/hall/rooms/alldata</p>
+  <p>ListCustomerswithallData-https://hall-booking-n72k.onrender.com/api/hall/customers/alldata</p>
+  <p>BookingCount- https://hall-booking-n72k.onrender.com/api/hall/count/alldata</p>
+</div>
+    
   `;
   res.send(`${welcomeMessage}<br><br>${apiLinks}`);
+  
 });
 
 app.use('/api/hall',router)
@@ -34,3 +45,5 @@ app.listen(PORT, ()=>{
 
     console.log('The backend app is listening with PORT', PORT);
 })
+
+
